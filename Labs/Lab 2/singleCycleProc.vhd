52ea095bf61dd87acee60fa93e039a88b981e55e
Sherman <113470498+sherman-0477@users.noreply.github.com>
2025-03-05 13:23:40 -0500

    Did stuff

--- Labs/Lab 2/singleCycleProc.vhd
+++ Labs/Lab 2/singleCycleProc.vhd
@@ -13,3 +13,34 @@ entity singleCycleProc is
 end singleCycleProc;
 
 architecture rtl of singleCycleProc is
+	component controlUnit is
+	port(i_Op : in std_logic_vector(5 downto 0);
+			o_RegDst, o_ALUSrc, o_MemtoReg, o_RegWrite, o_MemRead, o_MemWrite, o_Branch, o_ALUOp0, o_ALUOp1, o_jump : out std_logic
+			);
+	end component;
+	
+	component ALU is
+	port(
+		Operand1, Operand2 : in std_logic_vector(7 downto 0);
+		OpSel : in std_logic_vector(2 downto 0);
+		OpRes : out std_logic_vector(7 downto 0);
+		Zero, Overflow : out std_logic
+		);
+	end component;
+	
+	component mux8_2to1 is
+	port(
+		A_8bit, B_8bit : in std_logic_vector(7 downto 0);
+		Sel_8bit : in std_logic_vector;
+		O_8bit : out std_logic_vector(7 downto 0)
+	);
+	end component;
+	
+	component ALUControlUnit is
+	port(
+		i_Funct : in std_logic_vector(5 downto 0);
+		i_ALUOp : in std_logic_vector(1 downto 0);
+		o_Operation : out std_logic_vector(2 downto 0)
+	);
+	end component;
+		
